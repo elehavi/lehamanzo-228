dd8134282eadbc37877fd43e516acf349b62ec5d
Ella Lehavi <elehavi@ucsc.edu>
2025-05-23 17:19:57 -0700

    Model compiles

--- src/test/scala/TPU/TPUModel.scala
+++ src/test/scala/TPU/TPUModel.scala
@@ -1,5 +1,10 @@
+package tpu
 
-object TPUModel {
+import scala.collection.mutable.ArrayBuffer
+/* TODO: uncomment when TPU is parametrized and ready for testing
+
+object MatMulModel {
+  // Taken from Ella's HW4
   type Matrix = Seq[Seq[Int]]
   def findValAt(p: TPUParams, a: Matrix, b: Matrix, ai: Int, bj: Int): Int = {
     assert(p.aCols == p.bRows)
@@ -20,4 +25,45 @@ object TPUModel {
     outMat 
 
   }
+}
+*/
+
+// TODO: create parametrized version
+class fixedTPUModel {
+  type Matrix = Seq[Seq[Int]]
+  // represents the output/input of overall TPU
+  // format: (systolic array, outRight, outBottom)
+  type TPUIO = (Matrix, Array[Int], Array[Int])
+
+  // format of element of sysarray: (currResult, outRight, outBottom)
+  // represent the systolic array
+  val sysArray = Array.fill(2)(Array.fill(2)(Array(0,0,0)))
+
+  // process one more cycle of inputs. 
+  def progressOneCycle(inTop: Seq[Int], inLeft: Seq[Int]): Unit = {
+    // for each row and column, if you are on the top/left edge, take input from module
+    // else, take input from previous mac.
+    // multiply and accumulate.
+    for (i <- 0 until 2) { 
+      for (j <- 0 until 2) {
+        var it = 0
+        var il = 0
+        if(j==0) {
+          it = inTop(i)
+        } else {
+          it = sysArray(j-1)(i)(2)
+          }
+
+        if(i==0) {
+          it = inLeft(j)
+          } else {
+            it = sysArray(j)(i-1)(2)
+            }
+        val currResult = it * il + sysArray(j)(i)(0)
+        sysArray(j)(i) = Array(currResult, il, it)
+      }
+    }
+  }
+
+
 }
