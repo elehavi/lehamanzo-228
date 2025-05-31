b291db8a03ca88b02c9f457981ff8562eed2187a
bmanzo1 <bmanzo1@git.ucsc.edu>
2025-05-31 15:33:18 -0700

    added naive sparse implementation and rng MM test

--- src/main/scala/TPU/TPU.scala
+++ src/main/scala/TPU/TPU.scala
@@ -25,220 +25,6 @@ case class TPUParams (inputWidth: Int, outputWidth: Int, systolicArrayWidth: Int
     val cCols: Int = n
 }
 
-class TPU_fixed(p: TPUParams) extends Module {
-    val io = IO(new Bundle {
-        val inTopNE = Flipped(Decoupled(SInt(p.iw.W)))
-        val inTopNW = Flipped(Decoupled(SInt(p.iw.W)))
-        val inLeftNW = Flipped(Decoupled(SInt(p.iw.W)))
-        val inLeftSW = Flipped(Decoupled(SInt(p.iw.W)))
-
-        val clear = Input(Bool())
-
-        val outNE = Decoupled(SInt(p.ow.W))
-        val outNW = Decoupled(SInt(p.ow.W))
-        val outSE = Decoupled(SInt(p.ow.W))
-        val outSW = Decoupled(SInt(p.ow.W))
-    })
-    // TODO: Parameterize generation of MACS, hardcode for now
-    val macNE = Module(new MAC(macParams(p.iw, p.ow))) // inLeft, inTop, outRight, outBottom, outResult
-    val macNW = Module(new MAC(macParams(p.iw, p.ow)))
-    val macSE = Module(new MAC(macParams(p.iw, p.ow)))
-    val macSW = Module(new MAC(macParams(p.iw, p.ow)))
-
-
-    // ----------------------------------------------- Systolic Array IO -----------------------------------------------
-    // IO Data and Valid to Systolic Array Input
-    macNE.io.inTop.bits := io.inTopNE.bits
-    macNW.io.inTop.bits := io.inTopNW.bits
-    macNW.io.inLeft.bits := io.inLeftNW.bits
-    macSW.io.inLeft.bits := io.inLeftSW.bits
-
-    macNE.io.inTop.valid   := io.inTopNE.valid
-    macNW.io.inTop.valid   := io.inTopNW.valid
-    macNW.io.inLeft.valid  := io.inLeftNW.valid
-    macSW.io.inLeft.valid  := io.inLeftSW.valid
-
-    // IO Data and Valid from Systolic Array Output
-    io.outNE.valid := macNE.io.outResult.valid
-    io.outNW.valid := macNW.io.outResult.valid
-    io.outSE.valid := macSE.io.outResult.valid
-    io.outSW.valid := macSW.io.outResult.valid
-
-    io.outNE.bits := macNE.io.outResult.bits
-    io.outNW.bits := macNW.io.outResult.bits
-    io.outSE.bits := macSE.io.outResult.bits
-    io.outSW.bits := macSW.io.outResult.bits
-
-    // IO ready to Systolic Array Output
-    macNE.io.outResult.ready := io.outNE.ready
-    macNW.io.outResult.ready := io.outNW.ready
-    macSE.io.outResult.ready := io.outSE.ready
-    macSW.io.outResult.ready := io.outSW.ready
-
-    // Clear Inputs for Systolic Array
-    macNW.io.clear := io.clear
-    macNE.io.clear := io.clear
-    macSE.io.clear := io.clear
-    macSW.io.clear := io.clear
-
-    // ----------------------------------------------- Systolic Array Internal Wiring -----------------------------------------------
-    // Ready-Valid connections between MACs
-    macNW.io.outRight.ready  := macNE.io.inLeft.ready
-    macNW.io.outBottom.ready := macSW.io.inTop.ready
-    macSW.io.outRight.ready  := macSE.io.inLeft.ready
-    macNE.io.outBottom.ready := macSE.io.inTop.ready
-
-    macNE.io.inLeft.valid := macNW.io.outRight.valid
-    macSE.io.inLeft.valid := macSW.io.outRight.valid    
-    macSW.io.inTop.valid := macNW.io.outBottom.valid
-    macSE.io.inTop.valid := macNE.io.outBottom.valid
-    
-    // Data Connections between MACS
-    macNE.io.inLeft.bits := macNW.io.outRight.bits
-    macSE.io.inLeft.bits := macSW.io.outRight.bits
-    macSW.io.inTop.bits := macNW.io.outBottom.bits
-    macSE.io.inTop.bits := macNE.io.outBottom.bits
-
-    // Defaults
-    // east edge drains to nowhere
-    macNE.io.outRight.ready   := true.B   
-    macSE.io.outRight.ready   := true.B
-    // south edge drains to nowhere
-    macSW.io.outBottom.ready  := true.B
-    macSE.io.outBottom.ready  := true.B
-
-    io.inTopNE.ready  := true.B
-    io.inTopNW.ready  := true.B
-    io.inLeftNW.ready := true.B
-    io.inLeftSW.ready := true.B        
-}
-
-class Top_vec_io(p: TPUParams) extends Module {
-    val io = IO(new Bundle {
-        
-        val clear = Input(Bool())
-        // Vector input and output matrices
-        val in = Flipped(Decoupled(new Bundle {
-            val a = Vec(p.aRows, Vec(p.aCols, SInt(p.iw.W)))
-            val b = Vec(p.bRows, Vec(p.bCols, SInt(p.iw.W)))
-        }))
-        val out = Valid(Vec(p.aRows, Vec(p.bCols, SInt(p.ow.W))))
-
-    })
-    // 2x2 fixed Systolic Array
-    val TPU = Module(new TPU_fixed(p))
-
-    // Matrix Storage
-    val aReg = Reg(Vec(p.aRows, Vec(p.aCols, SInt(p.iw.W))))
-    val bReg = Reg(Vec(p.bRows, Vec(p.bCols, SInt(p.iw.W))))
-    // Initialize output mat to zeros
-    val cReg = RegInit(VecInit.tabulate(p.aRows)(_ =>
-                VecInit.fill(p.bCols)(0.S(p.ow.W))))
-
-    // -1 is the idle state
-    val writeCycle = new Counter(6)    // holds -1,0,1,2,3,4
-
-    // Registers representing times to read from MACs
-    val nwmacReadsRemaining = RegInit(0.U(2.W))
-    val nemacReadsRemaining = RegInit(0.U(2.W))
-    val swmacReadsRemaining = RegInit(0.U(2.W))
-    val semacReadsRemaining = RegInit(0.U(2.W))
-
-    // When register is zero, MAC value is finalized
-    TPU.io.outNW.ready := nwmacReadsRemaining =/= 0.U
-    TPU.io.outNE.ready := nemacReadsRemaining =/= 0.U
-    TPU.io.outSW.ready := swmacReadsRemaining =/= 0.U
-    TPU.io.outSE.ready := semacReadsRemaining =/= 0.U
-
-    // When outresult is valid and nonzero and it can still be read, read it out
-    when (TPU.io.outNW.fire) { cReg(0)(0) := TPU.io.outNW.bits ; nwmacReadsRemaining := nwmacReadsRemaining - 1.U }
-    when (TPU.io.outNE.fire) { cReg(0)(1) := TPU.io.outNE.bits ; nemacReadsRemaining := nemacReadsRemaining - 1.U }
-    when (TPU.io.outSW.fire) { cReg(1)(0) := TPU.io.outSW.bits ; swmacReadsRemaining := swmacReadsRemaining - 1.U }
-    when (TPU.io.outSE.fire) { cReg(1)(1) := TPU.io.outSE.bits ; semacReadsRemaining := semacReadsRemaining - 1.U }
-    
-    // Defaults
-
-    // IO Data and Valid to Systolic Array Input
-    TPU.io.inTopNE.valid := false.B
-    TPU.io.inTopNW.valid := false.B
-    TPU.io.inLeftNW.valid := false.B
-    TPU.io.inLeftSW.valid := false.B
-
-    TPU.io.inTopNE.bits := 0.S
-    TPU.io.inTopNW.bits := 0.S
-    TPU.io.inLeftNW.bits := 0.S
-    TPU.io.inLeftSW.bits := 0.S
-
-    io.out.bits  := cReg
-    io.out.valid := false.B
-    io.in.ready := true.B
-
-    TPU.io.clear := io.clear
-
-    switch(writeCycle.value) {
-    // Flashes input matrices to local registers in state -1
-        is(0.U){
-            aReg := io.in.bits.a
-            bReg := io.in.bits.b
-            writeCycle.inc()  
-        }
-        is(1.U) {
-            // First Write
-            nwmacReadsRemaining := 2.U
-            TPU.io.inTopNW.valid := true.B
-            TPU.io.inLeftNW.valid := true.B
-
-            TPU.io.inTopNW.bits := bReg(0)(0)
-            TPU.io.inLeftNW.bits := aReg(0)(0)
-
-            writeCycle.inc()  
-        }
-        is(2.U) {
-            // Last write
-            TPU.io.inTopNW.valid := true.B
-            TPU.io.inLeftNW.valid := true.B
-            TPU.io.inTopNW.bits := bReg(1)(0)
-            TPU.io.inLeftNW.bits := aReg(0)(1)
-
-            // First Write
-            nemacReadsRemaining := 2.U
-            swmacReadsRemaining := 2.U
-            TPU.io.inTopNE.valid := true.B
-            TPU.io.inLeftSW.valid := true.B
-            TPU.io.inTopNE.bits := bReg(0)(1)
-            TPU.io.inLeftSW.bits := aReg(1)(0)
-
-
-            writeCycle.inc()  
-        }
-        is(3.U) {
-            // Last write
-            semacReadsRemaining := 2.U
-            TPU.io.inTopNE.valid := true.B
-            TPU.io.inLeftSW.valid := true.B
-            TPU.io.inTopNE.bits := bReg(1)(1)
-            TPU.io.inLeftSW.bits := aReg(1)(1)
-
-            writeCycle.inc()  
-        }
-        is(4.U) {
-            when (nemacReadsRemaining === 0.U && swmacReadsRemaining === 0.U) {
-                writeCycle.inc()  
-            }
-        }
-        is(5.U) {
-            // Last read
-            when (nwmacReadsRemaining === 0.U && semacReadsRemaining === 0.U) {
-                writeCycle.inc()  
-            }
-        }
-    }
-    io.out.bits := cReg
-    io.out.valid := writeCycle.value === 4.U && nwmacReadsRemaining === 0.U && nemacReadsRemaining  === 0.U && swmacReadsRemaining  === 0.U && semacReadsRemaining  === 0.U
-
-    TPU.io.clear := io.clear
-}
-
 class TPU_parameterized(p: TPUParams) extends Module {
     val io = IO(new Bundle {
         val clear  = Input(Bool())
@@ -317,136 +103,6 @@ class TPU_parameterized(p: TPUParams) extends Module {
     }
 }
 
-class Top_fixed_TPU_parameterized(p: TPUParams) extends Module {
-    val io = IO(new Bundle {
-        
-        val clear = Input(Bool())
-        // Vector input and output matrices
-        val in = Flipped(Decoupled(new Bundle {
-            val a = Vec(p.aRows, Vec(p.aCols, SInt(p.iw.W)))
-            val b = Vec(p.bRows, Vec(p.bCols, SInt(p.iw.W)))
-        }))
-        val out = Valid(Vec(p.aRows, Vec(p.bCols, SInt(p.ow.W))))
-
-    })
-    // 2x2 fixed Systolic Array
-    val TPU = Module(new TPU_parameterized(p))
-
-    // Matrix Storage
-    val aReg = Reg(Vec(p.aRows, Vec(p.aCols, SInt(p.iw.W))))
-    val bReg = Reg(Vec(p.bRows, Vec(p.bCols, SInt(p.iw.W))))
-    // Initialize output mat to zeros
-    val cReg = RegInit(VecInit.tabulate(p.aRows)(_ =>
-                VecInit.fill(p.bCols)(0.S(p.ow.W))))
-
-    // -1 is the idle state
-    val writeCycle = new Counter(6)    // holds -1,0,1,2,3,4
-
-    // When register is zero, MAC value is finalized
-    val prevNW = RegNext(TPU.io.out(0)(0).bits)
-    val prevNE = RegNext(TPU.io.out(0)(1).bits)
-    val prevSW = RegNext(TPU.io.out(1)(0).bits)
-    val prevSE = RegNext(TPU.io.out(1)(1).bits)
-
-    val NWChanged = TPU.io.out(0)(0).bits =/= prevNW
-    val NEChanged = TPU.io.out(0)(1).bits =/= prevNE
-    val SWChanged = TPU.io.out(1)(0).bits =/= prevSW
-    val SEChanged = TPU.io.out(1)(1).bits =/= prevSE
-
-    val nwmacReadsRemaining = RegInit(0.U(2.W))
-    val nemacReadsRemaining = RegInit(0.U(2.W))
-    val swmacReadsRemaining = RegInit(0.U(2.W))
-    val semacReadsRemaining = RegInit(0.U(2.W))
-
-    // When outresult is valid and nonzero and it can still be read, read it out
-    when (NWChanged && TPU.io.out(0)(0).valid) { cReg(0)(0) := TPU.io.out(0)(0).bits ; nwmacReadsRemaining := nwmacReadsRemaining - 1.U }
-    when (NEChanged && TPU.io.out(0)(1).valid) { cReg(0)(1) := TPU.io.out(0)(1).bits ; nemacReadsRemaining := nemacReadsRemaining - 1.U }
-    when (SWChanged && TPU.io.out(1)(0).valid) { cReg(1)(0) := TPU.io.out(1)(0).bits ; swmacReadsRemaining := swmacReadsRemaining - 1.U }
-    when (SEChanged && TPU.io.out(1)(1).valid) { cReg(1)(1) := TPU.io.out(1)(1).bits ; semacReadsRemaining := semacReadsRemaining - 1.U }
-
-
-    // When register is zero, MAC value is finalized
-    TPU.io.out(0)(0).ready := nwmacReadsRemaining =/= 0.U
-    TPU.io.out(0)(1).ready := nemacReadsRemaining =/= 0.U
-    TPU.io.out(1)(0).ready := swmacReadsRemaining =/= 0.U
-    TPU.io.out(1)(1).ready := semacReadsRemaining =/= 0.U
-    
-    // Defaults
-
-    // IO Data and Valid to Systolic Array Input
-    TPU.io.inTop(0).valid := false.B
-    TPU.io.inTop(1).valid := false.B
-    TPU.io.inLeft(0).valid := false.B
-    TPU.io.inLeft(1).valid := false.B
-
-    TPU.io.inTop(0).bits := 0.S
-    TPU.io.inTop(1).bits := 0.S
-    TPU.io.inLeft(0).bits := 0.S
-    TPU.io.inLeft(1).bits := 0.S
-
-    io.out.bits  := cReg
-    io.out.valid := false.B
-    io.in.ready := true.B
-
-    TPU.io.clear := io.clear
-
-    switch(writeCycle.value) {
-    // Flashes input matrices to local registers in state -1
-        is(0.U){
-            aReg := io.in.bits.a
-            bReg := io.in.bits.b
-            writeCycle.inc()  
-        }
-        is(1.U) {
-            // First Write
-            TPU.io.inLeft(0).valid := true.B
-            TPU.io.inTop(0).valid := true.B
-
-            TPU.io.inLeft(0).bits := aReg(0)(0)
-            TPU.io.inTop(0).bits := bReg(0)(0)
-
-            writeCycle.inc()  
-        }
-        is(2.U) {
-            // Last write
-            TPU.io.inLeft(0).valid := true.B
-            TPU.io.inTop(0).valid := true.B
-            TPU.io.inLeft(0).bits := aReg(0)(1)
-            TPU.io.inTop(0).bits := bReg(1)(0)
-
-            // First Write
-            TPU.io.inLeft(1).valid := true.B
-            TPU.io.inTop(1).valid := true.B
-            TPU.io.inLeft(1).bits := aReg(1)(0)
-            TPU.io.inTop(1).bits := bReg(0)(1)
-
-
-            writeCycle.inc()  
-        }
-        is(3.U) {
-            // Last write
-            TPU.io.inLeft(1).valid := true.B
-            TPU.io.inTop(1).valid := true.B
-            TPU.io.inLeft(1).bits := aReg(1)(1)
-            TPU.io.inTop(1).bits := bReg(1)(1)
-
-            writeCycle.inc()  
-        }
-        is(4.U) {
-            writeCycle.inc()  
-
-        }
-        is(5.U) {
-            // Last read
-            writeCycle.inc()  
-        }
-    }
-    io.out.bits := cReg
-    io.out.valid := writeCycle.value === 5.U 
-
-    TPU.io.clear := io.clear
-}
-
 class Top_parameterized(p: TPUParams) extends Module {
     val io = IO(new Bundle {
         
@@ -466,9 +122,24 @@ class Top_parameterized(p: TPUParams) extends Module {
     // Local memory to flash input matrices to in idle state
     val aReg = Reg(Vec(p.aRows, Vec(p.aCols, SInt(p.iw.W))))
     val bReg = Reg(Vec(p.bRows, Vec(p.bCols, SInt(p.iw.W))))
+
     // Initialize output mat to zeros
     val cReg = RegInit(VecInit.tabulate(p.aRows)(_ =>
                        VecInit.fill(p.bCols)(0.S(p.ow.W))))
+    // Representaion Matrices
+    val aRep = Reg(Vec(p.aRows, Vec(p.aCols, Bool())))
+    val bRep = Reg(Vec(p.bRows, Vec(p.bCols, Bool())))
+
+    val aRepWire = Wire(Vec(p.aRows, Vec(p.aCols, Bool())))
+    val bRepWire = Wire(Vec(p.bRows, Vec(p.bCols, Bool())))
+
+    // Create Representation Matrices
+    for (row <- 0 until p.aRows; col <- 0 until p.aCols) {
+        aRepWire(row)(col) := aReg(row)(col) =/= 0.S
+    }
+    for (row <- 0 until p.bRows; col <- 0 until p.bCols) {
+        bRepWire(row)(col) := bReg(row)(col) =/= 0.S
+    }
 
     // -----------------------------------------------Output Read Logic-----------------------------------------------
 
@@ -514,10 +185,17 @@ class Top_parameterized(p: TPUParams) extends Module {
         // When in send state and valid window, assign matrix element to corresponding
         // systolic data input based on clock cycle
         TPU.io.inLeft(i).valid := (state === TPUState.writing) && sendWindow
-        TPU.io.inLeft(i).bits  := aReg(i)(writeCycle.value - i.U)
-
+        when(sendWindow && aRepWire(i)(writeCycle.value - i.U)) {
+            TPU.io.inLeft(i).bits    := aReg(i)(writeCycle.value - i.U)
+        } .otherwise {
+            TPU.io.inLeft(i).bits    := 0.S
+        }
         TPU.io.inTop(i).valid := (state === TPUState.writing) && sendWindow
-        TPU.io.inTop(i).bits  := bReg(writeCycle.value - i.U)(i)
+        when(sendWindow && bRepWire(writeCycle.value - i.U)(i)) {
+            TPU.io.inTop(i).bits   := bReg(writeCycle.value - i.U)(i)
+        } .otherwise {
+            TPU.io.inTop(i).bits  := 0.S
+        }
     }
 
     // ---------------------------------------------------Defaults---------------------------------------------------
--- src/test/scala/TPU/TPUTester.scala
+++ src/test/scala/TPU/TPUTester.scala
@@ -4,6 +4,7 @@ import chisel3._
 import chiseltest._
 import chisel3.experimental.BundleLiterals._
 import org.scalatest.flatspec.AnyFlatSpec
+import scala.util.Random
 
 
 class MACTester extends AnyFlatSpec with ChiselScalatestTester {
@@ -172,189 +173,6 @@ it should "do one round of multiplication, clear, and do another round" in {
   }
 }
 
-class fixedTPUModelTester extends AnyFlatSpec with ChiselScalatestTester {
-  behavior of "fixedTPUModel"
-  it should "handle one round of inputs" in {
-    val ftpu = new fixedTPUModel()
-    ftpu.progressOneCycle(Seq(1,2), Seq(1,2))
-    val expected = Array(Array(Array(1,1,1), Array(0,0,2)), Array(Array(0,2,0), Array(0,0,0)))
-    for (row <- 0 until 2) {
-      for (col <- 0 until 2) {
-        for (elt <- 0 until 3) {
-          assert(ftpu.sysArray(row)(col)(elt) == expected(row)(col)(elt))
-        }
-      }
-    }
-  }
-  it should "complete simple matrix multiplication" in {
-    val ftpu = new fixedTPUModel()
-    ftpu.progressOneCycle(Seq(1,0), Seq(1,0))
-    ftpu.progressOneCycle(Seq(1,2), Seq(1,2))
-    ftpu.progressOneCycle(Seq(0,1), Seq(0,1))
-    val expected = Array(Array(Array(2,0,0), Array(3,1,1)), Array(Array(3,1,1), Array(4,2,2)))
-    for (r <- 0 until 2) {
-      for (c <- 0 until 2) {
-        for (e <- 0 until 3) {
-          assert(ftpu.sysArray(r)(c)(e) == expected(r)(c)(e))
-        }
-      }
-    }
-    
-  }
-}
-
-
-class fixedTPUTester extends AnyFlatSpec with ChiselScalatestTester {
-behavior of "TPU_fixed"
-  it should "Multiply the first inputs" in {
-    val p = new TPUParams(8, 32, 2, 2, 2, 2)
-    test(new TPU_fixed(p)).withAnnotations(Seq(WriteVcdAnnotation)) { dut =>
-      dut.io.clear.poke(false.B)
-      // Ready to Consume from NW MAC
-      dut.io.outNW.ready.poke(true.B)
-      // Matrix A row 1, element 1
-      dut.io.inTopNW.bits.poke(5.S)
-      dut.io.inTopNW.valid.poke(true.B)
-      // Matrix B col 1, element 1
-      dut.io.inLeftNW.bits.poke(1.S)
-      dut.io.inLeftNW.valid.poke(true.B)
-
-      dut.clock.step(1)
-
-      // check NW = 1*5
-      dut.io.outNW.valid.expect(true.B)
-      dut.io.outNW.bits.expect(5.S)
-
-      // Ready to consume from NE and SW MAC
-      dut.io.outNE.ready.poke(true.B)
-      dut.io.outSW.ready.poke(true.B)
-      // Matrix A row 1, element 2
-      dut.io.inTopNW.bits.poke(6.S)
-      dut.io.inTopNW.valid.poke(true.B)
-      // Matrix A row 2, element 1
-      dut.io.inTopNE.bits.poke(7.S)
-      dut.io.inTopNE.valid.poke(true.B)
-      // Matrix B col 1, element 1
-      dut.io.inLeftNW.bits.poke(2.S)
-      dut.io.inLeftNW.valid.poke(true.B)
-      // Matrix B col 2, element 1
-      dut.io.inLeftSW.bits.poke(3.S)
-      dut.io.inLeftSW.valid.poke(true.B)
-
-      dut.clock.step(1)
-
-      // First cycle Inputs no longer valid
-      dut.io.inTopNW.valid.poke(false.B)
-      dut.io.inLeftNW.valid.poke(false.B)
-
-      // check NW = 1*5 + 2*6
-      dut.io.outNW.valid.expect(true.B)
-      dut.io.outNW.bits.expect(17.S)
-      // Check that NE = 1*7
-      dut.io.outNE.valid.expect(true.B)
-      dut.io.outNE.bits.expect(7.S)
-      // Check that SW = 5*3
-      dut.io.outSW.valid.expect(true.B)
-      dut.io.outSW.bits.expect(15.S)
-
-      // Matrix A row 2 element 2
-      dut.io.inTopNE.bits.poke(8.S)
-      dut.io.inTopNE.valid.poke(true.B)
-      // Matrix B col 2 element 2
-      dut.io.inLeftSW.bits.poke(4.S)
-      dut.io.inLeftSW.valid.poke(true.B)
-
-      dut.clock.step(1)
-      // Second cycle inputs no longer valid
-      dut.io.inTopNE.valid.poke(false.B)
-      dut.io.inLeftSW.valid.poke(false.B)
-
-      // check NW = 1*5 + 2*6
-      dut.io.outNW.valid.expect(false.B)
-      dut.io.outNW.bits.expect(17.S)
-
-      // Check that NE = 7*1 + 2*8
-      dut.io.outNE.valid.expect(true.B)
-      dut.io.outNE.bits.expect(23.S)
-      // Check that SW = 3*5 + 4*6
-      dut.io.outSW.valid.expect(true.B)
-      dut.io.outSW.bits.expect(39.S)
-
-      // Check that SE = 3*7
-      dut.io.outSE.valid.expect(true.B)
-      dut.io.outSE.bits.expect(21.S)
-
-      dut.clock.step(1)
-
-      // Check that SE = 3*7 + 8*4
-      dut.io.outSE.valid.expect(true.B)
-      dut.io.outSE.bits.expect(53.S) 
-
-    }
-  } 
-}
-
-class fixedvecTPUTester extends AnyFlatSpec with ChiselScalatestTester {
-  behavior of "Top_vec_io"
-
-  it should "output intermediate accumulations in correct cycles" in {
-    val p = new TPUParams(8, 32, 2, 2, 2, 2)
-
-    test(new Top_vec_io(p)).withAnnotations(Seq(WriteVcdAnnotation)) {
-      dut =>
-      /* ---------------- poke matrices ---------------- */
-      val aInit = Array(Array(1, 2), Array(3, 4))
-      val bInit = Array(Array(5, 7), Array(6, 8))
-
-      for (r <- 0 until p.aRows; c <- 0 until p.aCols)
-        dut.io.in.bits.a(r)(c).poke(aInit(r)(c).S(p.iw.W))
-      for (r <- 0 until p.bRows; c <- 0 until p.bCols)
-        dut.io.in.bits.b(r)(c).poke(bInit(r)(c).S(p.iw.W))
-
-      /* ------------- fire the bundle once ------------ */
-      dut.io.clear.poke(false.B)
-      dut.io.in.valid.poke(true.B)
-      dut.clock.step()            // cycle 0
-      dut.io.in.valid.poke(false.B)
-
-      /* ------------- pipeline latency ---------------- */
-      dut.clock.step(2)           // cycles 1–2
-
-      // NW = 1 × 5
-      dut.io.out.bits(0)(0).expect(5.S)
-
-      dut.clock.step()            // cycle 3
-
-      // NW = 1·5 + 2·6  = 17
-      dut.io.out.bits(0)(0).expect(17.S)
-      // NE = 1 × 7
-      dut.io.out.bits(0)(1).expect(7.S)
-      // SW = 3 × 5
-      dut.io.out.bits(1)(0).expect(15.S)
-
-      dut.clock.step()            // cycle 4
-
-      // NW unchanged
-      dut.io.out.bits(0)(0).expect(17.S)
-      // NE = 7 + 2·8 = 23
-      dut.io.out.bits(0)(1).expect(23.S)
-      // SW = 15 + 4·6 = 39
-      dut.io.out.bits(1)(0).expect(39.S)
-      // SE = 3 × 7 = 21
-      dut.io.out.bits(1)(1).expect(21.S)
-
-      dut.clock.step()            // cycle 5   ← SE first product just stored
-
-      /* extra cycle for SE’s second accumulation */
-      dut.clock.step()            // cycle 6
-
-      // SE = 21 + 4·8 = 53
-      dut.io.out.bits(1)(1).expect(53.S)
-    }
-  }
-}
-
-
 class TPUModelTester extends AnyFlatSpec with ChiselScalatestTester {
    behavior of "TPUModel"
   it should "handle one round of inputs" in {
@@ -475,6 +293,64 @@ class parameterizedTPUTester extends AnyFlatSpec with ChiselScalatestTester {
 
       for (r <- 0 until p.sysW; c <- 0 until p.sysW) {
         dut.io.out.bits(r)(c).expect(exp(r)(c).S(p.ow.W))
+      }
+    }
+  }
+  it should "multiply a 5x5 matrix" in {
+    val p = new TPUParams(8, 32, 5, 5, 5 ,5)
+
+    val rng = new Random(42)
+    def randomElem(): Int = rng.nextInt(1 << p.iw) - (1 << (p.iw - 1))
+
+    test(new Top_parameterized(p)).withAnnotations(Seq(WriteVcdAnnotation)) {
+      dut =>
+      /* ---------------- initialize random matrix inputs ---------------- */
+      val aInit: Array[Array[Int]] = Array.fill(p.aRows, p.aCols)(randomElem())
+      val bInit: Array[Array[Int]] = Array.fill(p.bRows, p.bCols)(randomElem())
+
+      val exp: Array[Array[Int]] = Array.ofDim[Int](p.aRows, p.bCols)
+      for (i <- 0 until p.aRows; j <- 0 until p.bCols) {
+        var sum = 0
+        for (k <- 0 until p.aCols) {
+          sum += aInit(i)(k) * bInit(k)(j)
+        }
+        exp(i)(j) = sum
+      }
+
+      /* ---------------- poke matrices ---------------- */
+      println("=== A matrix ===")
+      for (c <- 0 until p.aCols) {
+        for (r <- 0 until p.aRows) {
+          dut.io.in.bits.a(r)(c).poke(aInit(r)(c).S(p.iw.W))
+          print(f"${aInit(r)(c)}%4d ")
+        }
+        println()
+      }
+      println("=== B matrix ===")
+      for (c <- 0 until p.bCols) {
+        for (r <- 0 until p.bRows) {
+          dut.io.in.bits.b(r)(c).poke(bInit(r)(c).S(p.iw.W))
+          print(f"${bInit(r)(c)}%4d ")
+        }
+        println()
+      }
+            /* ------------- fire the bundle once ------------ */
+      dut.io.clear.poke(false.B)
+      dut.io.in.valid.poke(true.B)
+      dut.clock.step()            // cycle 0
+      dut.io.in.valid.poke(false.B)
+
+      /* ------------- pipeline latency ---------------- */
+      // Max writes + final reads
+      dut.clock.step(p.maxWrites + p.finalReads)           // cycles 1–2
+
+      println("=== C matrix ===")
+      for (c <- 0 until p.cCols) {
+        for (r <- 0 until p.cRows) {
+          dut.io.out.bits(r)(c).expect(exp(r)(c).S(p.ow.W))
+          print(f"${exp(r)(c)}%6d ")
+        }
+        println()
       }
     }
   }
