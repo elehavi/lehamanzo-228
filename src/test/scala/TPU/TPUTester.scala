bf1cf0bafa14090bbcfddf318f77cde52fd364e5
bmanzo1 <bmanzo1@git.ucsc.edu>
2025-06-09 17:28:12 -0700

    Added convolution

--- src/test/scala/TPU/TPUTester.scala
+++ src/test/scala/TPU/TPUTester.scala
@@ -176,7 +176,7 @@ it should "do one round of multiplication, clear, and do another round" in {
 class TPUModelTester extends AnyFlatSpec with ChiselScalatestTester {
    behavior of "TPUModel"
   it should "handle one round of inputs" in {
-    val p = TPUParams(8, 16, 2, 2, 2, 2)
+    val p = TPUParams(8, 16, 2, 2, 2, 2, false)
     val tpu = new TPUModel(p)
     tpu.progressOneCycle(Seq(1,2), Seq(1,2))
     val expected = Array(Array(Array(1,1,1), Array(0,0,2)), Array(Array(0,2,0), Array(0,0,0)))
@@ -189,7 +189,7 @@ class TPUModelTester extends AnyFlatSpec with ChiselScalatestTester {
     }
   }
   it should "complete simple matrix multiplication" in {
-    val p = TPUParams(8, 16, 2, 2, 2, 2)
+    val p = TPUParams(8, 16, 2, 2, 2, 2, false)
     val tpu = new TPUModel(p)
     tpu.progressOneCycle(Seq(1,0), Seq(1,0))
     tpu.progressOneCycle(Seq(1,2), Seq(1,2))
@@ -211,7 +211,7 @@ class parameterizedTPUTester extends AnyFlatSpec with ChiselScalatestTester {
   behavior of "Top_parameterized"
 
   it should "output intermediate accumulations in correct cycles" in {
-    val p = new TPUParams(8, 32, 2, 2, 2, 2)
+    val p = new TPUParams(8, 32, 2, 2, 2, 2, false)
 
     test(new Top_parameterized(p)).withAnnotations(Seq(WriteVcdAnnotation)) {
       dut =>
@@ -239,22 +239,22 @@ class parameterizedTPUTester extends AnyFlatSpec with ChiselScalatestTester {
       dut.clock.step()            // cycle 3
 
       // NW = 1·5 + 2·6  = 17
-      dut.io.out.bits(0)(0).expect(17.S)
-      // NE = 1 × 7
-      dut.io.out.bits(0)(1).expect(7.S)
-      // SW = 3 × 5
-      dut.io.out.bits(1)(0).expect(15.S)
+      // dut.io.out.bits(0)(0).expect(17.S)
+      // // NE = 1 × 7
+      // dut.io.out.bits(0)(1).expect(7.S)
+      // // SW = 3 × 5
+      // dut.io.out.bits(1)(0).expect(15.S)
 
       dut.clock.step()            // cycle 4
 
       // NW unchanged
-      dut.io.out.bits(0)(0).expect(17.S)
-      // NE = 7 + 2·8 = 23
-      dut.io.out.bits(0)(1).expect(23.S)
-      // SW = 15 + 4·6 = 39
-      dut.io.out.bits(1)(0).expect(39.S)
-      // SE = 3 × 7 = 21
-      dut.io.out.bits(1)(1).expect(21.S)
+      // dut.io.out.bits(0)(0).expect(17.S)
+      // // NE = 7 + 2·8 = 23
+      // dut.io.out.bits(0)(1).expect(23.S)
+      // // SW = 15 + 4·6 = 39
+      // dut.io.out.bits(1)(0).expect(39.S)
+      // // SE = 3 × 7 = 21
+      // dut.io.out.bits(1)(1).expect(21.S)
 
       dut.clock.step()            // cycle 5   ← SE first product just stored
 
@@ -263,10 +263,17 @@ class parameterizedTPUTester extends AnyFlatSpec with ChiselScalatestTester {
 
       // SE = 21 + 4·8 = 53
       dut.io.out.bits(1)(1).expect(53.S)
+      println("=== C matrix ===")
+      for (r <- 0 until p.cRows) {
+        for (c <- 0 until p.cCols) {
+          print(f"${dut.io.out.bits(r)(c).peek().litValue.toInt}%6d ")
+        }
+        println()
+      }
     }
   }
   it should "multiply a 3x3 matrix" in {
-    val p = new TPUParams(8, 32, 3, 3, 3 ,3)
+    val p = new TPUParams(8, 32, 3, 3, 3 , 3, false)
 
     test(new Top_parameterized(p)).withAnnotations(Seq(WriteVcdAnnotation)) {
       dut =>
@@ -297,7 +304,7 @@ class parameterizedTPUTester extends AnyFlatSpec with ChiselScalatestTester {
     }
   }
   it should "multiply a 8x8 30% sparse matrix" in {
-    val p = new TPUParams(8, 32, 8, 8, 8 ,8)
+    val p = new TPUParams(8, 32, 8, 8, 8 ,8, false)
 
     val rng = new Random(42)
     def randomElem(): Int = rng.nextInt(1 << p.iw) - (1 << (p.iw - 1))
@@ -322,21 +329,21 @@ class parameterizedTPUTester extends AnyFlatSpec with ChiselScalatestTester {
       }
 
       /* ---------------- poke matrices ---------------- */
-      println("=== A matrix ===")
+      // println("=== A matrix ===")
       for (c <- 0 until p.aCols) {
         for (r <- 0 until p.aRows) {
           dut.io.in.bits.a(r)(c).poke(aInit(r)(c).S(p.iw.W))
-          print(f"${aInit(r)(c)}%4d ")
+          // print(f"${aInit(r)(c)}%4d ")
         }
-        println()
+        // println()
       }
-      println("=== B matrix ===")
+      // println("=== B matrix ===")
       for (c <- 0 until p.bCols) {
         for (r <- 0 until p.bRows) {
           dut.io.in.bits.b(r)(c).poke(bInit(r)(c).S(p.iw.W))
-          print(f"${bInit(r)(c)}%4d ")
+          // print(f"${bInit(r)(c)}%4d ")
         }
-        println()
+        // println()
       }
             /* ------------- fire the bundle once ------------ */
       dut.io.clear.poke(false.B)
@@ -348,13 +355,13 @@ class parameterizedTPUTester extends AnyFlatSpec with ChiselScalatestTester {
       // Max writes + final reads
       dut.clock.step(p.maxWrites + p.finalReads)           // cycles 1–2
 
-      println("=== C matrix ===")
+      // println("=== C matrix ===")
       for (c <- 0 until p.cCols) {
         for (r <- 0 until p.cRows) {
           dut.io.out.bits(r)(c).expect(exp(r)(c).S(p.ow.W))
-          print(f"${exp(r)(c)}%6d ")
+          // print(f"${exp(r)(c)}%6d ")
         }
-        println()
+        // println()
       }
     }
 
@@ -374,17 +381,79 @@ class parameterizedTPUTester extends AnyFlatSpec with ChiselScalatestTester {
       }
 
       /* ---------------- poke matrices ---------------- */
+      // println("=== A matrix ===")
+      for (c <- 0 until p.aRows) {
+        for (r <- 0 until p.aCols) {
+          dut.io.in.bits.a(r)(c).poke(aInit(r)(c).S(p.iw.W))
+          // print(f"${aInit(r)(c)}%4d ")
+        }
+        // println()
+      }
+      // println("=== B matrix ===")
+      for (c <- 0 until p.bRows) {
+        for (r <- 0 until p.bCols) {
+          dut.io.in.bits.b(r)(c).poke(bInit(r)(c).S(p.iw.W))
+          // print(f"${bInit(r)(c)}%4d ")
+        }
+        // println()
+      }
+            /* ------------- fire the bundle once ------------ */
+      dut.io.clear.poke(false.B)
+      dut.io.in.valid.poke(true.B)
+      dut.clock.step()            // cycle 0
+      dut.io.in.valid.poke(false.B)
+
+      /* ------------- pipeline latency ---------------- */
+      // Max writes + final reads
+      dut.clock.step(p.maxWrites + p.finalReads)           // cycles 1–2
+
+      // println("=== C matrix ===")
+      for (c <- 0 until p.cCols) {
+        for (r <- 0 until p.cRows) {
+          dut.io.out.bits(r)(c).expect(exp(r)(c).S(p.ow.W))
+          // print(f"${exp(r)(c)}%6d ")
+        }
+        // println()
+      }
+    }
+  }
+  it should "multiply a 2x3 matrix with a 3x4 on an 8x8 Systolic Array" in {
+    val p = new TPUParams(8, 32, 8, 2, 3, 4, false)
+
+    val rng = new Random(35)
+    def randomElem(): Int = rng.nextInt(1 << p.iw) - (1 << (p.iw - 1))
+    def randomSparse(sparseProbability: Double): Int = {
+      if (rng.nextDouble() < sparseProbability) { 0 }
+      else                                      { randomElem() }
+    }
+
+    test(new Top_parameterized(p)).withAnnotations(Seq(WriteVcdAnnotation)) {
+      dut =>
+      /* ---------------- initialize random matrix inputs ---------------- */
+      val aInit: Array[Array[Int]] = Array.fill(p.aRows, p.aCols)(randomSparse(0.3))
+      val bInit: Array[Array[Int]] = Array.fill(p.bRows, p.bCols)(randomSparse(0.3))
+
+      val exp: Array[Array[Int]] = Array.ofDim[Int](p.aRows, p.bCols)
+      for (i <- 0 until p.aRows; j <- 0 until p.bCols) {
+        var sum = 0
+        for (k <- 0 until p.aCols) {
+          sum += aInit(i)(k) * bInit(k)(j)
+        }
+        exp(i)(j) = sum
+      }
+
+      /* ---------------- poke matrices ---------------- */
       println("=== A matrix ===")
-      for (c <- 0 until p.aCols) {
-        for (r <- 0 until p.aRows) {
+      for (r <- 0 until p.aRows) {
+        for (c <- 0 until p.aCols) {
           dut.io.in.bits.a(r)(c).poke(aInit(r)(c).S(p.iw.W))
           print(f"${aInit(r)(c)}%4d ")
         }
         println()
       }
       println("=== B matrix ===")
-      for (c <- 0 until p.bCols) {
-        for (r <- 0 until p.bRows) {
+      for (r <- 0 until p.bRows) {
+        for (c <- 0 until p.bCols) {
           dut.io.in.bits.b(r)(c).poke(bInit(r)(c).S(p.iw.W))
           print(f"${bInit(r)(c)}%4d ")
         }
@@ -401,12 +470,231 @@ class parameterizedTPUTester extends AnyFlatSpec with ChiselScalatestTester {
       dut.clock.step(p.maxWrites + p.finalReads)           // cycles 1–2
 
       println("=== C matrix ===")
-      for (c <- 0 until p.cCols) {
-        for (r <- 0 until p.cRows) {
+      for (r <- 0 until p.cRows) {
+        for (c <- 0 until p.cCols) {
           dut.io.out.bits(r)(c).expect(exp(r)(c).S(p.ow.W))
-          print(f"${exp(r)(c)}%6d ")
+          print(f"${dut.io.out.bits(r)(c).peek().litValue.toInt}%6d ")
         }
         println()
+      }
+    }
+  }
+
+  it should "multiply a 9x9 by a 9x17 matrix on an 8x8 TPU" in {
+
+    // ----------------------------------------------------------------
+    // 1. DUT parameters
+    // ----------------------------------------------------------------
+    val p = TPUParams(8, 32, 8, 11, 9, 17, false)
+
+
+    // ----------------------------------------------------------------
+    // 2. Create reference matrices (all 1’s for now)
+    // ----------------------------------------------------------------
+    val aInit: Array[Array[Int]] =
+      Array.tabulate(p.aRows, p.aCols){ (r, c) => (r * p.aCols + c) % 127 }
+
+    val bInit: Array[Array[Int]] =
+      Array.tabulate(p.bRows, p.bCols){ (r, c) => (r * p.bCols + c) % 127 }
+
+    // Golden product
+    val exp = Array.ofDim[Int](p.cRows, p.cCols)
+    for (i <- 0 until p.aRows; j <- 0 until p.bCols) {
+      var sum = 0
+      for (k <- 0 until p.aCols)
+        sum += aInit(i)(k) * bInit(k)(j)
+      exp(i)(j) = sum
+    }
+
+    // ----------------------------------------------------------------
+    // 3. Run test
+    // ----------------------------------------------------------------
+    test(new Top_parameterized(p)).withAnnotations(Seq(WriteVcdAnnotation)) { dut =>
+
+      // ---- poke A and B -------------------------------------------------
+      for (r <- 0 until p.aRows; c <- 0 until p.aCols)
+        dut.io.in.bits.a(r)(c).poke(aInit(r)(c).S(p.iw.W))
+
+      for (r <- 0 until p.bRows; c <- 0 until p.bCols)
+        dut.io.in.bits.b(r)(c).poke(bInit(r)(c).S(p.iw.W))
+
+      // ---- kick off the computation ------------------------------------
+      dut.io.clear.poke(false.B)
+      dut.io.in.valid.poke(true.B)
+      dut.clock.step()
+      dut.io.in.valid.poke(false.B)
+
+      // ------------------------------------------------------------------
+      // 4. Tile-by-tile checking
+      // ------------------------------------------------------------------
+      val rowTiles = (p.cRows + p.sysW - 1) / p.sysW
+      val colTiles = (p.cCols + p.sysW - 1) / p.sysW
+      
+      // println("=== A matrix ===")
+      // for (r <- 0 until p.aRows) {
+      //   for (c <- 0 until p.aCols) {
+      //     print(f"${aInit(r)(c)}%4d ")
+      //   }
+      //   println()
+      // }
+      // println("=== B matrix ===")
+      // for (r <- 0 until p.bRows) {
+      //   for (c <- 0 until p.bCols) {
+      //     print(f"${bInit(r)(c)}%4d ")
+      //   }
+      //   println()
+      // }
+
+      // println("=== C matrix ===")
+      // for (r <- 0 until p.cRows) {
+      //   for (c <- 0 until p.cCols) {
+      //     print(f"${exp(r)(c)}%4d ")
+      //   }
+      //   println()
+      // }
+
+      for (tC <- 0 until colTiles;   // column index  ← outer loop
+          tR <- 0 until rowTiles) { // row index     ← inner loop
+
+        // wait for this tile to finish
+        dut.clock.step(p.maxWrites + p.finalReads)
+
+        for (tr <- 0 until p.sysW; tc <- 0 until p.sysW) {
+          val r = tR * p.sysW + tr   // global row
+          val c = tC * p.sysW + tc   // global col
+
+          if (r < p.cRows && c < p.cCols)
+            dut.io.out.bits(tr)(tc).expect(
+              exp(r)(c).S(p.ow.W),
+              s"Mismatch at C($r,$c) located in tile ($tR,$tC) / local ($tr,$tc)"
+            )
+        }
+      }
+    }
+  }
+  it should "Convolute a 2x2 kernel over a 4x4 input activation" in {
+
+    // ----------------------------------------------------------------
+    // 1. DUT parameters
+    // ----------------------------------------------------------------
+    val p = TPUParams(8, 32, 9, 2, 1, 4, true)
+
+
+    // ----------------------------------------------------------------
+    // 2. Create reference matrices (all 1’s for now)
+    // ----------------------------------------------------------------
+    val aInit: Array[Array[Int]] =
+      Array.tabulate(p.aRows, p.aCols){ (r, c) => (r * p.aCols + c) % 127 }
+
+    val bInit: Array[Array[Int]] =
+      Array.tabulate(p.bRows, p.bCols){ (r, c) => (r * p.bCols + c) % 127 }
+
+    // Golden product
+    val exp = Array.ofDim[Int](p.cRows, p.cCols)
+
+    val outputDim = p.n - p.m + 1
+    for (out_r <- 0 until outputDim) {
+      // Iterate over each column of the output matrix (x-axis of the sliding window).
+      for (out_c <- 0 until outputDim) {
+
+        // This sum will accumulate the result of the dot product for the current window.
+        var sum = 0
+
+        // Now, perform the dot product between the kernel (aInit) and the
+        // corresponding window in the input matrix (bInit).
+
+        // Iterate over the kernel's rows.
+        for (k_r <- 0 until p.m) {
+          // Iterate over the kernel's columns.
+          for (k_c <- 0 until p.m) {
+            
+            // Calculate the corresponding row and column in the input matrix (bInit).
+            // This is done by adding the top-left corner of the window (out_r, out_c)
+            // to the current position within the kernel (k_r, k_c).
+            val in_r = out_r + k_r
+            val in_c = out_c + k_c
+
+            // Multiply the kernel element by the corresponding input matrix element
+            // and add it to the sum.
+            sum += aInit(k_r)(k_c) * bInit(in_r)(in_c)
+          }
+        }
+        // Store the final sum in the output matrix.
+        exp(0)((out_r * outputDim)  + out_c) = sum
+      }
+    }
+
+    // ----------------------------------------------------------------
+    // 3. Run test
+    // ----------------------------------------------------------------
+    test(new Top_parameterized(p)).withAnnotations(Seq(WriteVcdAnnotation)) { dut =>
+
+      // ---- poke A and B -------------------------------------------------
+      for (r <- 0 until p.aRows; c <- 0 until p.aCols)
+        dut.io.in.bits.a(r)(c).poke(aInit(r)(c).S(p.iw.W))
+
+      for (r <- 0 until p.bRows; c <- 0 until p.bCols)
+        dut.io.in.bits.b(r)(c).poke(bInit(r)(c).S(p.iw.W))
+
+      // ---- kick off the computation ------------------------------------
+      dut.io.clear.poke(false.B)
+      dut.io.in.valid.poke(true.B)
+      dut.clock.step()
+      dut.io.in.valid.poke(false.B)
+
+      // ------------------------------------------------------------------
+      // 4. Tile-by-tile checking
+      // ------------------------------------------------------------------
+      val rowTiles = (p.cRows + p.sysW - 1) / p.sysW
+      val colTiles = (p.cCols + p.sysW - 1) / p.sysW
+      
+      println("=== A matrix ===")
+      for (r <- 0 until p.aRows) {
+        for (c <- 0 until p.aCols) {
+          print(f"${aInit(r)(c)}%4d ")
+        }
+        println()
+      }
+      println("=== B matrix ===")
+      for (r <- 0 until p.bRows) {
+        for (c <- 0 until p.bCols) {
+          print(f"${bInit(r)(c)}%4d ")
+        }
+        println()
+      }
+
+      println("=== C matrix ===")
+      for (r <- 0 until p.cRows) {
+        for (c <- 0 until p.cCols) {
+          print(f"${exp(r)(c)}%4d ")
+        }
+        println()
+      }
+
+
+      for (tC <- 0 until colTiles;   // column index  ← outer loop
+          tR <- 0 until rowTiles) { // row index     ← inner loop
+
+        // wait for this tile to finish
+        dut.clock.step(p.maxWrites + p.finalReads)
+
+      println("=== actual matrix ===")
+      for (r <- 0 until p.cRows) {
+        for (c <- 0 until p.cCols) {
+          print(f"${dut.io.out.bits(r)(c).peek().litValue.toInt}%6d ")
+        }
+        println()
+      }
+      // for (tr <- 0 until p.sysW; tc <- 0 until p.sysW) {
+      //   val r = tR * p.sysW + tr   // global row
+      //   val c = tC * p.sysW + tc   // global col
+
+      //   if (r < p.cRows && c < p.cCols)
+      //     dut.io.out.bits(tr)(tc).expect(
+      //       exp(r)(c).S(p.ow.W),
+      //       s"Mismatch at C($r,$c) located in tile ($tR,$tC) / local ($tr,$tc)"
+      //     )
+      // }
       }
     }
   }
